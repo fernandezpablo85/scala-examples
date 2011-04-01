a85b9599097dc5a11892caa0db382137326cf44d
Pablo Fernandez <fernandezpablo85@gmail.com>
2011-04-01 00:53:16 -0300

    Initialized customers and shop in main class

--- src/main/scala/org/actors/example/Main.scala
+++ src/main/scala/org/actors/example/Main.scala
@@ -2,19 +2,27 @@ package org.actors.example
 
 object Main
 {
-  val MAX_CLEINTS = 25;
+  val MAX_CLIENTS = 25;
   
   def main (args : Array[String])
   {
     println("Starting simulation");
+
+    // Generate Customer actors and start them
     val customers = generateCustormers();
+    customers.foreach { customer => customer.start() }
+
+    // Create the shop and put it to work
+    val shop = new Shop("Starbucks")
+    shop.start;
   }
   
   private def generateCustormers() = {
     import java.util.Random;
     
-    val total = new Random().nextInt(Main.MAX_CLEINTS);
-    println("Creating " + total + " customers");
-    Vector.fill(total)(new Customer)
+    val total = new Random().nextInt(Main.MAX_CLIENTS);
+    println("Creating "+total+" customers");
+
+    (1 to total).map(new Customer(_))
   }
 }
