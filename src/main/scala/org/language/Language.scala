6e216002c84363b161ceb2dbc92ee89835c2f326
Pablo Fernandez <fernandezpablo85@gmail.com>
2011-04-07 23:26:19 -0300

    Functions examples

--- src/main/scala/org/language/Language.scala
+++ src/main/scala/org/language/Language.scala
@@ -1,5 +1,7 @@
 package org.language;
 
+import java.io._
+
 class MyString(val string : String) {
 
   def dasherize() = {
@@ -13,11 +15,42 @@ object LanguageMain
 
   def main (args : Array[String])
   {
+    testClosures
     testImplicits
-  }
+  } 
 
   def testImplicits ()
   {
     println("pablo".dasherize);
+  }
+
+  def testClosures ()
+  {
+    val lang = "Scala"
+
+    using ("pablo.txt") { file =>
+      file println "hola archivito"
+      file println "estoy usando "+lang+" para escribirte"
+    } 
+  }
+
+  def using (name : String)(op : PrintWriter => Unit) {
+    var f : File = null
+    var w : PrintWriter = null
+
+    try
+    { 
+      f = new File(name)
+      w = new PrintWriter(f)
+      op(w) 
+    }
+    catch
+    {
+      case e : Exception => println("Something went wrong");
+    }
+    finally
+    {
+      w.close
+    }
   }
 }
